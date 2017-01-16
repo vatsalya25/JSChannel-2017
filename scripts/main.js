9edbc00816697b2444c67a0cfd6dd9a7e7dbff87
prakharrustagi <prakhar_rustagi@mckinsey.com>
2017-01-16 17:26:38 +0530

    hotel and map layout

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,14 @@ function initMap() {
   var hotelposition = {lat: 12.972006, lng: 77.594871};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 16,
-    center: hotelposition
+    zoomControl: true,
+    scrollwheel: false,
+    clickableIcons: false,
+    draggable:false,
+    center: hotelposition,
+    disableDoubleClickZoom: true
   });
+
   var marker = new google.maps.Marker({
     position: hotelposition,
     map: map,
