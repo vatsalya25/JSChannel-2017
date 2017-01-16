4b3515b772a6f5e50c7a9bd91e5a1fe3e5db7a9c
prakharrustagi <prakhar_rustagi@mckinsey.com>
2017-01-16 15:23:54 +0530

    map added

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,12 @@
 function initMap() {
-        var position = {lat: 12.972006, lng: 77.5926823};
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-
-        var marker = new google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-
-      }
+  var hotelposition = {lat: 12.972006, lng: 77.594871};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 16,
+    center: hotelposition
+  });
+  var marker = new google.maps.Marker({
+    position: hotelposition,
+    map: map,
+    title: 'JSChannel Conference'
+  });
+}
